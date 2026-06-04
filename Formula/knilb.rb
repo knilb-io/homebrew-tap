86676bed321ae567741feef2dc6dd60a00742142
knilb <noreply@knilb.io>
2026-06-04 21:05:22 +0000

    chore: bump knilb to v0.5.0

--- Formula/knilb.rb
+++ Formula/knilb.rb
@@ -1,29 +1,29 @@
 class Knilb < Formula
   desc "Knilb command-line interface"
   homepage "https://github.com/knilb-io/knilb-dist"
-  version "0.4.0"
+  version "0.5.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.4.0/knilb-macos-arm64.tar.gz"
-      sha256 "71d0620f34bddd5ec6c66e0b610818729ef02aaa6e34e4d79a7000d8fb77ef8d"
+      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.5.0/knilb-macos-arm64.tar.gz"
+      sha256 "51fe4ec605ad16a967e4bf2360d522ff58040ff78c483315fd4e11752952e206"
     end
 
     on_intel do
-      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.4.0/knilb-macos-x86_64.tar.gz"
-      sha256 "0a0ff1e560998ed8b2fa61ea320682c5992adce963851909833d85fa0b79dc77"
+      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.5.0/knilb-macos-x86_64.tar.gz"
+      sha256 "8f8583bd44c0dea5b09187e28dd56de8758866952240c0e6a7768197943139c8"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.4.0/knilb-linux-arm64.tar.gz"
-      sha256 "42759820792951951448e9dff4342024c15a5693cdc68d20105784427de03f02"
+      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.5.0/knilb-linux-arm64.tar.gz"
+      sha256 "c3d3b3165383ff8c96dd9363335d538d9475592bea536c73f6d92967793ffbee"
     end
 
     on_intel do
-      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.4.0/knilb-linux-x86_64.tar.gz"
-      sha256 "4ae00b6467abf16bc557cb91bab874d8e3a99a6c95aa6719d4d011014a8d9d3d"
+      url "https://github.com/knilb-io/knilb-dist/releases/download/v0.5.0/knilb-linux-x86_64.tar.gz"
+      sha256 "8573a6653e2e61daf7a6a252eea35268a0df7c5478e43ff53a3d3d6a7a72d3bf"
     end
   end
 
